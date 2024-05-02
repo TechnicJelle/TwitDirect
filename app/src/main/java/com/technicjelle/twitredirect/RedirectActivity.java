24afa1479b0d824b755c3a4817bc74931a0d8caa
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-05-02 02:31:51 +0200

    Split up MainActivity into RedirectActivity
    RedirectActivity is now only the ephemeral redirector activity.
    It does nothing else, like opening the settings menu.
    That's all handles by the MainActivity.

---
+++ app/src/main/java/com/technicjelle/twitredirect/RedirectActivity.java
@@ -0,0 +1,48 @@
+package com.technicjelle.twitredirect;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RedirectActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// ATTENTION: This was auto-generated to handle app links.
+		Intent appLinkIntent = getIntent();
+		String appLinkAction = appLinkIntent.getAction();
+		Uri appLinkData = appLinkIntent.getData();
+		// END ATTENTION
+
+		// If appLinkData is null, the app was probably opened directly, without a link, so cancel the activity
+		if (appLinkData == null) {
+			Log.d("TwitRedirect", "appLinkData is null");
+			finish();
+			return;
+		}
+
+		Log.d("TwitRedirect", "appLinkAction: " + appLinkAction);
+		Log.d("TwitRedirect", "appLinkData: " + appLinkData);
+
+		Toast.makeText(this, "Redirecting to Twitter...", Toast.LENGTH_SHORT).show();
+
+		String path = appLinkData.toString().replaceFirst("^https?://[^/]+", ""); // Remove scheme and domain/host
+		Log.d("TwitRedirect", "path: " + path);
+
+		Uri redirectURI = Uri.parse(appLinkData.getScheme() + "://twitter.com/" +
+				path.replaceFirst("^/", "")); // Remove leading slash
+		Log.d("TwitRedirect", "redirectURI: " + redirectURI);
+
+		Intent intent = new Intent(appLinkAction, redirectURI);
+		startActivity(intent);
+
+		// Finish the activity so it doesn't stay in the back stack
+		finish();
+	}
+}
