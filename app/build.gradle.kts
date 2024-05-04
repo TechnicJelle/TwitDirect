37622029e770c0cc881199332cfac1bcc1eb3f78
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-05-04 04:07:08 +0200

    Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 		applicationId = "com.technicjelle.twitdirect"
 		minSdk = 26
 		targetSdk = 34
-		versionCode = 1
-		versionName = "0.1"
+		versionCode = 2
+		versionName = "1.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
--- app/src/main/java/com/technicjelle/twitdirect/RedirectActivity.java
+++ app/src/main/java/com/technicjelle/twitdirect/RedirectActivity.java
@@ -3,7 +3,6 @@ package com.technicjelle.twitdirect;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,22 +21,16 @@ public class RedirectActivity extends AppCompatActivity {
 
 		// If appLinkData is null, the app was probably opened directly, without a link, so cancel the activity
 		if (appLinkData == null) {
-			Log.d("TwitDirect", "appLinkData is null");
 			finish();
 			return;
 		}
 
-		Log.d("TwitDirect", "appLinkAction: " + appLinkAction);
-		Log.d("TwitDirect", "appLinkData: " + appLinkData);
-
 		Toast.makeText(this, "Redirecting to Twitter...", Toast.LENGTH_SHORT).show();
 
 		String path = appLinkData.toString().replaceFirst("^https?://[^/]+", ""); // Remove scheme and domain/host
-		Log.d("TwitDirect", "path: " + path);
 
 		Uri redirectURI = Uri.parse(appLinkData.getScheme() + "://twitter.com/" +
 				path.replaceFirst("^/", "")); // Remove leading slash
-		Log.d("TwitDirect", "redirectURI: " + redirectURI);
 
 		Intent intent = new Intent(appLinkAction, redirectURI);
 		startActivity(intent);
