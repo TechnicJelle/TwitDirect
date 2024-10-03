94be8b9b3bfe56950a26554da0dba6de3adea2b8
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-10-04 01:46:31 +0200

    Update build.gradle.kts
    Bump version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 		minSdk = 26
 		targetSdk = 34
 		versionCode = 2
-		versionName = "1.0"
+		versionName = "1.1"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
