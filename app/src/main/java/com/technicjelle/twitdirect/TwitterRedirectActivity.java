0a83bd143ddb57084d79eed4b2beb99ffdedb563
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-01-04 22:13:27 +0100

    Rename RedirectActivity to TwitterRedirectActivity
    In preparation for more Redirect Activities for other websites

--- app/src/main/java/com/technicjelle/twitdirect/TwitterRedirectActivity.java
+++ app/src/main/java/com/technicjelle/twitdirect/TwitterRedirectActivity.java
@@ -7,7 +7,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RedirectActivity extends AppCompatActivity {
+public class TwitterRedirectActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
